53ccaa32958ac596847e7dad63bddd92aba466be
Travis Northcutt <travis@travisnorthcutt.com>
2024-11-21 17:43:49 -0700

    change base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scales",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
